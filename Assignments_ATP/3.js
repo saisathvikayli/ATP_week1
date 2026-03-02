9274cf4106ea21b1f8955bad025d5a897ff9167c
saisathvikayli <saisathvik.ayli@gmail.com>
2026-03-02 20:10:50 +0530

    Fix formatting issues in marks sum calculation

--- Assignments_ATP/3.js
+++ Assignments_ATP/3.js
@@ -1,7 +1,8 @@
-//given that marks=[90,78,65,98]
+//given that marks=[90,78,65,98] find the sum of marks array
 let marks = [90, 78, 65, 98]
 let sum = 0
 let i;
 for (i = 0; i < marks.length; i++)
     sum += marks[i]
 console.log("Sum of the marks is ", sum)
+
